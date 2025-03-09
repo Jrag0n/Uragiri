f431a12744d50288a51faefee4066ac25d1a66bf
John <john.kotoucek@gmail.com>
2025-03-09 20:35:48 +0000

    Metadata OneFile, Chain attack mid added, main amended (wip)

--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46b99760df1bf5b1ba1d72cc09783f92df61650c905e2982f4e26cd7358a3777
-size 52256
+oid sha256:b4d2914e77de683fa49c0aaa723c5b1d6f54cdceaa1fe72fdf2a87b515a008b6
+size 52577
--- classes.py
+++ classes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e360ec0c0f96c44283fc288928d9056dc5f46074e1ab8c7beb59efe19e97b760
-size 55677
+oid sha256:ec42c0ba11a050fe0c11b1ed18da908abae5f27f2fcb11ab3079544696d2c2e5
+size 56188
